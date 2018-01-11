f89f319d160da06fbed2009071aa540f6bcb8916
biello <bellusa@qq.com>
2018-01-11 14:59:26 +0800

    增加单元测试

--- src/test/java/ind/kobe/person/controller/PersonControllerTest.java
+++ src/test/java/ind/kobe/person/controller/PersonControllerTest.java
@@ -2,13 +2,18 @@ package ind.kobe.person.controller;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import ind.kobe.person.bean.Person;
 
 
 /**
@@ -21,6 +26,8 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @AutoConfigureMockMvc
 public class PersonControllerTest {
 
+	private final static Logger logger = LoggerFactory.getLogger(PersonControllerTest.class);
+	
     @Autowired
     private MockMvc mvc;
 
@@ -29,6 +36,17 @@ public class PersonControllerTest {
         mvc.perform(MockMvcRequestBuilders.get("/persons"))
                 .andExpect(MockMvcResultMatchers.status().isOk());  //测试返回状态
 //              .andExpect(MockMvcResultMatchers.content().string("abc"));  //测试返回内容
+    }
+    
+    @Test
+    public void personAdd() throws Exception {
+    	Person person = new Person();
+    	person.setName("xukang");
+    	person.setAge(17);
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post("/person", person))
+                .andExpect(MockMvcResultMatchers.status().isOk()).andReturn();  //测试返回状态
+//              .andExpect(MockMvcResultMatchers.content().string("abc"));  //测试返回内容
+        logger.info(mvcResult.getResponse().getContentAsString());
     }
 
 }
